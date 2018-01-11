6f94799f6b15df3f3fdb992155e99b6d6ec32c70
Valeriy Popov <complexity@yandex-team.ru>
2018-01-11 23:32:39 +0300

    Add task 4.2 solution

--- src/Chapter4/Containers.hs
+++ src/Chapter4/Containers.hs
@@ -1,9 +1,18 @@
 module Chapter4.Containers (
-    insertF
+    insertF,
+    deleteF,
+    adjustF
     ) where
 
 import qualified Data.Map as M
 
 insertF :: (Ord k) => k -> a -> M.Map k a -> M.Map k a
-insertF key value = M.alter (\(Nothing) -> (Just value)) key 
+insertF key value = M.alter (\(Nothing) -> (Just value)) key
 
+deleteF :: (Ord k) => k -> M.Map k a -> M.Map k a
+deleteF key = M.alter (\(Just _) -> Nothing) key
+
+adjustF :: (Ord k) => (a -> a) -> k -> M.Map k a -> M.Map k a
+adjustF transform key = M.alter (\value -> case value of
+                                           Nothing -> Nothing
+                                           (Just value) -> Just (transform value)) key
--- test/unit/Chapter4/ContainersSpec.hs
+++ test/unit/Chapter4/ContainersSpec.hs
@@ -2,6 +2,7 @@ module Chapter4.ContainersSpec where
     
 import Test.Hspec
 import Chapter4.Containers
+import Data.Char
 import qualified Data.Map as M
 
 spec :: Spec
@@ -10,4 +11,16 @@ spec = do
 
         it "Insert in map" $ do
             (insertF "key2" "value2" (M.singleton "key1" "value1")) 
+            `shouldBe` (M.fromList [("key1", "value1"), ("key2", "value2")])
+
+        it "Delete in map" $ do
+            (deleteF "key2" (M.fromList [("key1", "value1"), ("key2", "value2")]))
+            `shouldBe` (M.singleton "key1" "value1")
+            
+        it "Adjust existing value in map" $ do
+            (adjustF (\str -> map toUpper str) "key2" (M.fromList [("key1", "value1"), ("key2", "value2")]))
+            `shouldBe` (M.fromList [("key1", "value1"), ("key2", "VALUE2")])
+
+        it "Adjust missed value in map" $ do
+            (adjustF (\str -> map toUpper str) "key3" (M.fromList [("key1", "value1"), ("key2", "value2")]))
             `shouldBe` (M.fromList [("key1", "value1"), ("key2", "value2")])
